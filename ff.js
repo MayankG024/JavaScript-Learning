8416dbb6ed4518b5d9ece5f7c02a0abfcf17628d
Mayank Gupta <100185011+MayankG024@users.noreply.github.com>
2024-06-29 19:51:06 +0530

    Add files via upload

---
+++ ff.js
@@ -0,0 +1,10 @@
+const GITHUB_API = "https://api.github.com/usershttps://github.com/MayankG024/";
+
+const news = fetch(GITHUB_API);
+console.log(news);
+news.then(function (data) {
+  console.log(data);
+});
+console.log("simmy and timmy are dumbasses");
+console.log("simmy and timmy are dumboos");
+console.log("mayank is a don ");
---
+++ script.js
@@ -0,0 +1,19 @@
+const personArray = ["harkirat", "raman", "sirji"];
+const genderArray = ["male", "male", "female"];
+
+for (let i = 0; i < personArray.length; i++) {
+  if (genderArray[i] == "female") {
+    console.log(personArray[i]);
+  }
+}
+const user1 = {
+  newname: "mayank",
+  age: 21,
+};
+const user2 = {
+  newname: "farook",
+};
+console.log(user1.newname);
+console.log(user2.newname);
+console.log("lets goo lets learn");
+console.log("lets goo lets learn");
